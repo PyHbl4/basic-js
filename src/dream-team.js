3e89431b294f7c342f75cd06794227ad67874ec7
PyHbl4 <witchercccp@gmail.com>
2022-05-02 18:21:05 +0500

    feat: finish some tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)){
+    return false;
+  }
+  let result = '';
+  let resultArr = [];
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      resultArr.push(element.trim()[0].toUpperCase());
+    }
+  });
+  resultArr.sort();
+  resultArr.forEach(element => {
+    result = `${result}${element}`;
+  })
+  console.log(result);
+  return result;
 }
 
 module.exports = {
